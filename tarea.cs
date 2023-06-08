2584221fedde2843910814d87a7e26bd5df60bb6
AdanSmith <smith.paredes.13@gmail.com>
2023-06-08 19:08:50 -0300

    Clase tarea

---
+++ tarea.cs
@@ -0,0 +1,16 @@
+namespace tarea;
+
+public class Tarea{
+    private int id;
+    private string descripcion;
+    private int duracion;
+    public int Id { get => id; set => id = value; }
+    public string Descripcion { get => descripcion; set => descripcion = value; }
+    public int Duracion { get => duracion; set => duracion = value; }
+
+    public void mostrarTarea(){
+        Console.WriteLine($"ID de la tarea: {id}");
+        Console.WriteLine($"Descripción: {descripcion}");
+        Console.WriteLine($"Duración: {duracion}\n");
+    }
+}
