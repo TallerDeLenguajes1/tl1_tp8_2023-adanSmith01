41bfdb776db4cd7e83c1a9392c9f06533294a863
AdanSmith <smith.paredes.13@gmail.com>
2023-06-08 23:22:21 -0300

    Ejercicio 01 terminado

--- Program.cs
+++ Program.cs
@@ -6,13 +6,42 @@ internal class Program
     {
         List<Tarea> tareasPendientes = new List<Tarea>();
         List<Tarea> tareasRealizadas = new List<Tarea>();
-        
+        Tarea tareaBuscada = new Tarea();
+        bool continuar = true;
+        string confirmar = String.Empty;
+        string filtro = String.Empty;
+
         // Generando el numero N de forma aleatoria
-        Random r = new Random();
-        int N = r.Next(2, 5);
+        int N = new Random().Next(2, 5);
         cargarTareasPendientes(2, tareasPendientes);
         moverTareas(tareasPendientes, tareasRealizadas);
-        
+
+        Console.Write("\n¿Desea buscar alguna tarea pendiente? Si/No: ");
+        confirmar = Console.ReadLine();
+        if(confirmar.ToLower() == "si"){
+            while(continuar){
+                Console.Write("\nIngrese la descripción de la tarea a buscar: ");
+                filtro = Console.ReadLine();
+                tareaBuscada = buscarTareaPorDescripcion(tareasPendientes, filtro);
+                if(String.IsNullOrEmpty(tareaBuscada.Descripcion)){
+                    Console.WriteLine("\nNo existe la tarea pendiente que desea encontrar\n");
+                } else{
+                    Console.WriteLine("\n===========TAREA BUSCADA===========\n");
+                    tareaBuscada.MostrarTarea();
+                }
+                Console.Write("\n¿Desea buscar otra tarea pendiente? Si/No: ");
+                confirmar = Console.ReadLine();
+                if(confirmar.ToLower() != "si"){
+                    continuar = false;
+                }
+            }
+        }
+
+        if(tareasRealizadas.Count > 0){
+            guardarSumarioEmpleado(tareasRealizadas);
+        } else{
+            Console.WriteLine("\nNo puede guardarse el sumario de horas trabajadas porque no hay tareas realizadas\n");
+        }
     }
 
     // Función para cargar las tareas pendientes
@@ -51,4 +80,34 @@ internal class Program
         }
     }
 
+    public static Tarea buscarTareaPorDescripcion(List<Tarea> pendientes, string descripcion){
+        Tarea tareaABuscar = new Tarea();
+        foreach (var tareaP in pendientes){
+            if(tareaP.Descripcion == descripcion){
+                tareaABuscar = tareaP;
+            }
+        }
+
+        return tareaABuscar;
+    }
+
+    public static void mostrarTareas(List<Tarea> listaTareas){
+        if(listaTareas.Count == 0){
+            Console.WriteLine("\nNo hay tareas guardadas\n");
+        } else{
+            foreach(var tarea in listaTareas){
+                tarea.MostrarTarea();
+            }
+        }
+    }
+
+    public static void guardarSumarioEmpleado(List<Tarea> realizadas){
+        int totalHoras = 0;
+        StreamWriter dato = new StreamWriter("sumario.txt");
+        foreach(var tareaR in realizadas){
+            totalHoras += tareaR.Duracion;
+        }
+        dato.WriteLine($"Sumario: {totalHoras}");
+        dato.Close();
+    }
 }
