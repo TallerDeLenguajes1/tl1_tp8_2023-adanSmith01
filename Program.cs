2a645a3d2a37aca20566a7bf97e4b71d87c8cc6d
adanSmith01 <smith.paredes.13@gmail.com>
2023-06-13 00:19:53 -0300

    Modificaciones

--- Program.cs
+++ Program.cs
@@ -2,16 +2,29 @@
 {
     private static void Main(string[] args)
     {
-        string path = @"C:\Users\smith\Downloads\Taller 1";
-        List<string> listaArchivos = Directory.GetFiles(path).ToList();
+        string path;
+        Console.Write("Ingrese una ruta valida: ");
+        path = Console.ReadLine();
 
-        StreamWriter indexador = new StreamWriter("index.csv");
-        for(int i = 0; i < listaArchivos.Count; i++){
-            // El método Path.GetFileWithoutExtension() devuelve solo el nombre del archivo
-            // El método Path.GetExtension() devuelve la extensión del archivo
-            indexador.WriteLine($"{i},{Path.GetFileNameWithoutExtension(listaArchivos[i])},{Path.GetExtension(listaArchivos[i])}");
+        if(!Directory.Exists(path)){
+            Console.WriteLine("ERROR. Ruta invalida\n");
+        } else{
+            List<string> listaArchivos = Directory.GetFiles(path).ToList();
+            Console.WriteLine("Lista de archivos");
+            listaArchivos.ForEach(Console.WriteLine);
+            using(StreamWriter indexador = new StreamWriter("index.csv")){
+
+                for(int i = 0; i < listaArchivos.Count; i++){
+                    // El método Path.GetFileWithoutExtension() devuelve solo el nombre del archivo
+                    // El método Path.GetExtension() devuelve la extensión del archivo
+                    indexador.WriteLine($"{i},{Path.GetFileNameWithoutExtension(listaArchivos[i])},{Path.GetExtension(listaArchivos[i])}");
+                }
+
+                indexador.Close();
+                indexador.Dispose();
+            }
+
+
         }
-
-        indexador.Close();
     }
 }
